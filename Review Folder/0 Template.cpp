132b1236cc2a02fea1d66bad40693eb8e6720cb7
nimitbdave <nimitbdave@gmail.com>
2020-10-25 15:40:29 -0700

    Create 0 Template.cpp

---
+++ Review Folder/0 Template.cpp
@@ -0,0 +1,27 @@
+//This is a comment.
+
+/*
+multi-line
+*/
+
+//Libraries
+
+#include <iostream>
+#include <vectors>
+#include <string>
+#include <fstream>
+
+using namespace std
+
+//Functions
+
+int main() {
+  
+  
+  
+  
+  
+  
+  return 0;
+
+}

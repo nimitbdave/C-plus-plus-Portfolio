8545442ac2a62835753e4672a940b5a3064af2cc
nimitbdave <nimitbdave@gmail.com>
2020-10-26 10:43:11 -0700

    Create 10 Functions.cpp

---
+++ Review Folder/10 Functions.cpp
@@ -0,0 +1,34 @@
+//This is a comment.
+
+/*
+multi-line
+*/
+
+//Libraries
+
+#include <iostream>
+#include <vectors>
+#include <string>
+#include <fstream>
+
+using namespace std
+
+//Functions
+
+int addNumbers(int firstNum, int secondNum = 0){ //set default value at 0, here.
+
+  int combinedValue = firstNum + secondNum; 
+  return combinedValue;
+  
+}
+
+int main() {
+  
+  
+  
+  
+  
+  
+  return 0;
+
+}

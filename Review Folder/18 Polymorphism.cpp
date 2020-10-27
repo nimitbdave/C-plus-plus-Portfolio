78a3c14b7e5e76b52bb73ff154cc77195d2c47ef
nimitbdave <nimitbdave@gmail.com>
2020-10-27 15:29:02 -0700

    Create 18 Polymorphism.cpp

---
+++ Review Folder/18 Polymorphism.cpp
@@ -0,0 +1,30 @@
+//This is a comment.
+
+/*
+multi-line
+*/
+
+//Libraries
+
+#include <iostream>
+#include <vectors>
+#include <string>
+#include <fstream>
+
+using namespace std
+
+class Animal { 
+  public: 
+    virtual void makeSound(   //We will use a virtual method because we are going to override it 
+                              //and treat them as animals.
+
+int main() {
+  
+  
+  
+  
+  
+  
+  return 0;
+
+}

a03ec68611f1b6106d54add27ffbf1c519eee89f
nimitbdave <nimitbdave@gmail.com>
2020-10-27 16:27:26 -0700

    Update 18 Polymorphism.cpp

--- Review Folder/18 Polymorphism.cpp
+++ Review Folder/18 Polymorphism.cpp
@@ -16,26 +16,53 @@ using namespace std
 //We will use a virtual method because we are going to override it and treat them as animals.
 class Animal { 
   public: 
-    virtual void makeSound() { cout << "The Animal says grr" << endl; }
-                              
+    virtual void makeSound() { cout << "The Animal says grr" << endl; }                              
 }; 
 
+//NOTE: NOT VIRTUAL
 class Dog : public Animal {
   public:
-    
+    void makeSound() { cout << "The Dog says woof" << endl; }
 };
+
 
 class Cat : public Animal {
   public: 
-  
+    void makeSound() { cout << "The Cat says meow" << endl; }
 };
-      
+
+//Abstract Data type.  
+class Car{
+  public: 
+    virtual int getNumWheels() = 0;
+    virtual int getNumDoors() = 0; 
+};
+
+//Create stationwagon class, which inherits from Car.  
+class StationWagon : public Car {
+  public: 
+    int getNumWheels(){ cout << "Station wagon has 4 wheels" << endl; }
+    int getNumDoors() { cout << "Station wagon has 4 doors" << endl; }
+    StationWagon() {} //Empty constructor
+    ~StationWagon();  //Destructor
+
 int main() {
   
+  Animal* pCat = new Cat;   //make a new animal, cat
+  Animal* pDog = new Dog;   //make a new animal, dog
   
+  pCat -> makeSound(); 
+  pDog -> makeSound(); 
   
+  Car* stationWagon = new StationWagon(); //create a stationwagon of type car, then create new s.wagon
   
-  
+  stationWagon -> getNumWheels(); //even though it is a car object, getNumWheels() works.  
+    
+  /* Output:  
+  The Cat says meow
+  The Dog says woof
+  Station wagon has 4 wheels 
+  */
   
   return 0;
 

6f876fe474846c0f0430af8997809e12954e500c
nimitbdave <nimitbdave@gmail.com>
2020-12-13 13:33:38 -0800

    Update 001 Data Structures, Bracket Sequences.cpp

--- HackerEarth/001 Data Structures, Bracket Sequences.cpp
+++ HackerEarth/001 Data Structures, Bracket Sequences.cpp
@@ -55,39 +55,39 @@ int main()
 				x = i + 1;
 			}
 
-	}// the idea of the above loop is increment l when we get '(' and decrement at ')' and note the point in x where l becomes the lowest. Let's take an example
+		}// the idea of the above loop is increment l when we get '(' and decrement at ')' and note the point in x where l becomes the lowest. Let's take an example
 
 //input : )()())((   at i=0,l=-1,y=-1,x=i+1=1  at i=1,l=0,no change in x & y as if condition is false  at i=2,l=-1  at i=3,l=0  at i=4,l=-1  at i=5,l=-2,y=-2,x=i+1=6  at i=6,l=-1  at i=7,l=0 end;
 
 //at x=6, l=-2 which is the lowest and if u observe that if we left-shift array to x(=6) positions, the input becomes balanced in O(n). 
 
-  reverse(s.begin(), s.begin()+x);
+		reverse(s.begin(), s.begin()+x);
 
-  reverse(s.begin()+x, s.end());
+		reverse(s.begin()+x, s.end());
 
-  reverse(s.begin(), s.end());
+		reverse(s.begin(), s.end());
 
 //above 3 lines left-shifts the array to 6 positions and the input becomes (()()()) {balanced}.
 
-	l=0;
+		l=0;
 
-	for(i=0;i<s.length();i++)
+		for(i=0;i<s.length();i++)
 
-	{
+		{
 
-		if(s[i]=='(')
+			if(s[i]=='(')
 
-			l++;
+				l++;
 
-		else
+			else
 
-			l--;
+				l--;
 
-		if(l==0)
+			if(l==0)
 
-			z++;
+				z++;
 
-	}
+		}
 
 //above loop increments l at '(' and decrement at ')' and counts the points where l=0 in z, notice that the points where z becomes zero, if u left-shift your input at that position
 
@@ -101,9 +101,9 @@ int main()
 
 //answer will be 4. //Hope u like it :) (Satwik Dash)
 
-cout << z;
+		cout << z;
 
-}
+	}
 
     return 0;
 
